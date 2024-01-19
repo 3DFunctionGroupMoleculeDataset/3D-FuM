a2438dc1f05aaee58680134f9c35ec61ffc1fc19
lajictw <ctw313903233@outlook.com>
2024-01-18 19:44:40 -0800

    changed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import * as path from 'path'
 
 export default defineConfig({
+    base:"vue3_page_test",
     resolve:{
         alias:{
             '@':path.resolve(__dirname,'src'),
